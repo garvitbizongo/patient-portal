3e755441f02d2da129a343d32920190372732468
Garvit Verma <garvit@bizongo.in>
2018-08-06 22:03:23 +0530

    Validation on patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,7 @@
 class Patient < ActiveRecord::Base
+  enum status: [:inactive, :active]
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :contact_number, presence: true, length: { minimum: 10, maximum: 15 }
 end
