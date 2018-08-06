87374d910cb9d4c034f14b99dbb1ff723b0c2717
Garvit Verma <garvit@bizongo.in>
2018-08-06 22:57:15 +0530

    Method of api auth

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,3 +1,36 @@
 class Api::ApiController < ActionController::Base
-  
+
+  def auth
+    unless valid_secret_token?
+      render json: { error: "Invalid Secret Token" }, status: :unauthorized
+      return
+    end
+
+    email, password = ActionController::HttpAuthentication::Basic::user_name_and_password(request)
+    patient = Patient.find_by_email(email)
+
+    if patient.blank?
+      render json: { error: "Patient with email: #{email} not found!"}, status: :not_found
+      return
+    end
+
+    if patient.inactive?
+      render json: { error: "Patient with email: #{email} is currently inactive!"}, status: :unauthorized
+      return
+    end
+
+    render json: { error: "Invalid Password" }, status: :unauthorized unless patient.valid_password?(password)
+
+    @logging_in_patient = patient
+  end
+
+  private
+
+  def valid_secret_token?
+    secret_token_received = request.headers['HTTP_SECRET_TOKEN']
+    return false unless secret_token_received
+
+    actual_secret_token = Rails.application.secrets.client_secret
+    secret_token_received.eql?(actual_secret_token)
+  end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -4,4 +4,19 @@ class Patient < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :contact_number, presence: true, length: { minimum: 10, maximum: 15 }
+
+  before_save :ensure_auth_token
+
+  def ensure_auth_token
+    return if auth_token.present?
+
+    self.auth_token = generate_auth_token
+  end
+
+  def generate_auth_token
+    loop do
+      token = SecureRandom.hex
+      break token unless Patient.where(auth_token: token).first
+    end
+  end
 end
