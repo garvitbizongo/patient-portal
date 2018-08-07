ee86342f7dafb44dac9465a987f6ab5d0f961afd
Garvit Verma <garvit@bizongo.in>
2018-08-07 09:19:37 +0530

    Update method to update the prescription request.

--- app/controllers/api/prescription_requests_controller.rb
+++ app/controllers/api/prescription_requests_controller.rb
@@ -5,6 +5,20 @@ class Api::PrescriptionRequestsController < Api::ApiController
     @prescription_requests = PrescriptionRequest.filter(filter_params)
   end
 
+  def update
+    @prescription_request = PrescriptionRequest.find_by_id(params[:id])
+
+    render json: { errors: "Prescription request not found for the given id" }, status: :bad_request if @prescription_request.blank?
+
+    @prescription_request.assign_attributes(update_params)
+
+    if @prescription_request.save
+      render json: { }, status: :ok
+    else
+      render json: { errors: @prescription_request.errors.full_messages }, status: :bad_request
+    end
+  end
+
   private
 
   def filter_params
@@ -12,6 +26,12 @@ class Api::PrescriptionRequestsController < Api::ApiController
       :patient_id,
       :prescription_id,
       :doctor_pharmacist_id
+    )
+  end
+
+  def update_params
+    params.permit(
+      :status
     )
   end
 end
